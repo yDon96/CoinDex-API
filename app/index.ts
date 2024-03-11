34b873da80c332d329e4578b68f0e50e3d48101f
Youssef Donadeo <you.donadeo@icloud.com>
2024-03-11 14:50:48 +0100

    Add Global Error Handler to router

--- app/index.ts
+++ app/index.ts
@@ -2,10 +2,13 @@ import express from 'express'
 import { router as coinsRouterV1 } from './coins/v1/coins.router'
 import { router as countryRouterV1 } from './countries/v1/countries.router'
 import { swagger } from './swagger'
+import {globalErrorHandler} from "../middleware/globalErrorHandler";
 
 const result = express()
 
 result.use('/api/v1', [coinsRouterV1, countryRouterV1]);
+
+result.use(globalErrorHandler);
 swagger(result);
 
 module.exports = result
